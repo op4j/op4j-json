772d851066c91b72ddc75aefba8a4f2e23b5871f
Soraya Sánchez <soraya.schz@gmail.com>
2010-01-31 20:16:07 +0000

    Minor changes
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j-json@329 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/test/java/org/op4j/contrib/executables/functions/conversion/ToJsonStringTest.java
+++ src/test/java/org/op4j/contrib/executables/functions/conversion/ToJsonStringTest.java
@@ -1,355 +1,143 @@
 package org.op4j.contrib.executables.functions.conversion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import junit.framework.TestCase;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 import net.sf.json.JsonConfig;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.op4j.Op;
 import org.op4j.contrib.utils.json.CalendarJsonValueProcessor;
 
 public class ToJsonStringTest extends TestCase {
 
-//	@Test
-//	public final void test1() {
-//		
-//		ArrayList<Long> firstLongsList = new ArrayList<Long>();
-//    	firstLongsList.add(Long.valueOf(4));
-//    	firstLongsList.add(Long.valueOf(3));
-//    	firstLongsList.add(Long.valueOf(2));
-//    	firstLongsList.add(Long.valueOf(1));		
-//    	
-//    	ABean firstABean = new ABean("The string property value of ABean - firstBean", 
-//    			13, 
-//    			Integer.valueOf(22), 
-//    			firstLongsList, 
-//				new ArrayList<Object>(), 
-//				Calendar.getInstance());   
-//    	
-//    	ArrayList<Object> firstObjectsList = new ArrayList<Object>();
-//    	firstObjectsList.add(Long.valueOf(3));
-//    	firstObjectsList.add(firstABean);   
-//    	
-//    	ArrayList<Long> secondLongsList = new ArrayList<Long>();
-//    	secondLongsList.add(Long.valueOf(34));
-//    	
-//    	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
-//    	secondObjectsList.add("one");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	secondObjectsList.add("A string property in the secondObjectsList");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(new ArrayList<Long>());
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	
-//    	assertEquals(JSONSerializer.toJSON(Arrays.asList(Integer.valueOf(4))).toString(),
-//    			Op.on(Arrays.asList(Integer.valueOf(4))).exec(ToJsonString.forObject()).get());
-//    	
-//	}
-//
-//	@Test
-//	public final void test2() {
-//		
-//		JsonConfig jsonConfig = new JsonConfig();
-//	
-//		ArrayList<Long> firstLongsList = new ArrayList<Long>();
-//    	firstLongsList.add(Long.valueOf(4));
-//    	firstLongsList.add(Long.valueOf(3));
-//    	firstLongsList.add(Long.valueOf(2));
-//    	firstLongsList.add(Long.valueOf(1));		
-//    	
-//    	ABean firstABean = new ABean("The string property value of ABean - firstBean", 
-//    			13, 
-//    			Integer.valueOf(22), 
-//    			firstLongsList, 
-//				new ArrayList<Object>(), 
-//				Calendar.getInstance());   
-//    	
-//    	ArrayList<Object> firstObjectsList = new ArrayList<Object>();
-//    	firstObjectsList.add(Long.valueOf(3));
-//    	firstObjectsList.add(firstABean);   
-//    	
-//    	
-//    	ArrayList<Long> secondLongsList = new ArrayList<Long>();
-//    	secondLongsList.add(Long.valueOf(34));
-//    	
-//    	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
-//    	secondObjectsList.add("one");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	secondObjectsList.add("A string property in the secondObjectsList");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(new ArrayList<Long>());
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	
-//    	jsonConfig = new JsonConfig();
-//    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
-//    	assertEquals(JSONSerializer.toJSON(firstABean, jsonConfig).toString(), 
-//    			Op.on(firstABean).exec(ToJsonString.forObject()).get());    			
-//    	
-//	}
+	ArrayList<Long> firstLongsList = new ArrayList<Long>();
+	ABean firstABean;
+	ArrayList<Object> firstObjectsList = new ArrayList<Object>();
+	ArrayList<Long> secondLongsList = new ArrayList<Long>();
+	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
+	BBean firstBBean;
+	ABean secondABean;
+	
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		this.firstLongsList.add(Long.valueOf(4));
+		this.firstLongsList.add(Long.valueOf(3));
+		this.firstLongsList.add(Long.valueOf(2));
+		this.firstLongsList.add(Long.valueOf(1));
+		
+		this.firstABean = new ABean("The string property value of ABean - firstBean", 
+    			13, 
+    			Integer.valueOf(22), 
+    			this.firstLongsList, 
+				new ArrayList<Object>(), 
+				Calendar.getInstance());
+		
+		this.firstObjectsList.add(Long.valueOf(3));
+		this.firstObjectsList.add(this.firstABean);  
+		
+		this.secondLongsList.add(Long.valueOf(34));
+		
+		this.secondObjectsList.add("one");
+		this.secondObjectsList.add(Long.valueOf(45));
+		this.secondObjectsList.add(Calendar.getInstance());
+		this.secondObjectsList.add("A string property in the secondObjectsList");
+		this.secondObjectsList.add(Long.valueOf(45));
+		this.secondObjectsList.add(new ArrayList<Long>());
+		this.secondObjectsList.add(Calendar.getInstance());
+		
+		this.firstBBean = new BBean("bStringProperty", 
+    			34, 
+    			Integer.valueOf(4), 
+    			this.secondLongsList, 
+    			this.secondObjectsList, 
+    			Calendar.getInstance(), 
+    			"A transient string property", 
+    			34, 
+    			this.secondLongsList, 
+    			Calendar.getInstance(), 
+    			Calendar.getInstance());
+		
+		this.secondABean = new ABean("Test json - secondABean string property value", 
+    			10, 
+    			Integer.valueOf(5), 
+    			this.firstLongsList,
+    			this.firstObjectsList, 
+				Calendar.getInstance());
+	}
+	
+	@Test
+	public final void test1() {
+		assertEquals(JSONSerializer.toJSON(Arrays.asList(Integer.valueOf(4))).toString(),
+    			Op.on(Arrays.asList(Integer.valueOf(4))).exec(ToJsonString.fromObject()).get());    	
+	}
+
+	@Test
+	public final void test2() {
+		
+		JsonConfig jsonConfig = new JsonConfig();
+		jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
+    	assertEquals(JSONSerializer.toJSON(this.firstABean, jsonConfig).toString(), 
+    			Op.on(this.firstABean).exec(ToJsonString.fromObject()).get());    	    	
+	}
 
 	@Test
 	public final void test3() {
 		
 		JsonConfig jsonConfig = new JsonConfig();
-	
+		jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
+    	jsonConfig.registerJsonValueProcessor(GregorianCalendar.class, new CalendarJsonValueProcessor());
     	
-    	ArrayList<Long> secondLongsList = new ArrayList<Long>();
-    	secondLongsList.add(Long.valueOf(34));
-    	
-    	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
-    	secondObjectsList.add("one");
-    	secondObjectsList.add(Long.valueOf(45));
-    	secondObjectsList.add(Calendar.getInstance());
-    	secondObjectsList.add("A string property in the secondObjectsList");
-    	secondObjectsList.add(Long.valueOf(45));
-    	secondObjectsList.add(new ArrayList<Long>());
-//    	secondObjectsList.add(Calendar.getInstance());
-    	
-    	BBean firstBBean = new BBean("bStringProperty", 
-    			34, 
-    			Integer.valueOf(4), 
-    			secondLongsList, 
-    			secondObjectsList, 
-    			Calendar.getInstance(), 
-    			"A transient string property", 
-    			34, 
-    			secondLongsList, 
-    			Calendar.getInstance(), 
-    			Calendar.getInstance());
-    	
-    	
-    	jsonConfig = new JsonConfig();
-    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
-    	assertEquals(JSONSerializer.toJSON(firstBBean, jsonConfig).toString(), 
-    			Op.on(firstBBean).exec(ToJsonString.forObject()).get());   
-    			 
+    	assertEquals(JSONSerializer.toJSON(this.firstBBean, jsonConfig).toString(), 
+    			Op.on(this.firstBBean).exec(ToJsonString.fromObject()).get());     			 
 	}
 
-//	@Test
-//	public final void test4() {
-//		
-//		JsonConfig jsonConfig = new JsonConfig();
-//	
-//		ArrayList<Long> firstLongsList = new ArrayList<Long>();
-//    	firstLongsList.add(Long.valueOf(4));
-//    	firstLongsList.add(Long.valueOf(3));
-//    	firstLongsList.add(Long.valueOf(2));
-//    	firstLongsList.add(Long.valueOf(1));		
-//    	
-//    	ABean firstABean = new ABean("The string property value of ABean - firstBean", 
-//    			13, 
-//    			Integer.valueOf(22), 
-//    			firstLongsList, 
-//				new ArrayList<Object>(), 
-//				Calendar.getInstance());   
-//    	
-//    	ArrayList<Object> firstObjectsList = new ArrayList<Object>();
-//    	firstObjectsList.add(Long.valueOf(3));
-//    	firstObjectsList.add(firstABean);   
-//    	
-//    	
-//    	ArrayList<Long> secondLongsList = new ArrayList<Long>();
-//    	secondLongsList.add(Long.valueOf(34));
-//    	
-//    	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
-//    	secondObjectsList.add("one");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	secondObjectsList.add("A string property in the secondObjectsList");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(new ArrayList<Long>());
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	
-//    	BBean firstBBean = new BBean("bStringProperty", 
-//    			34, 
-//    			Integer.valueOf(4), 
-//    			secondLongsList, 
-//    			secondObjectsList, 
-//    			Calendar.getInstance(), 
-//    			"A transient string property", 
-//    			34, 
-//    			secondLongsList, 
-//    			Calendar.getInstance(), 
-//    			Calendar.getInstance());
-//    	
-//    	
-//    	
-//    			    	    	   	
-//    	jsonConfig = new JsonConfig();
-//    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
-//    	jsonConfig.setIgnoreTransientFields(false);
-//    	jsonConfig.setIgnoreJPATransient(false);
-//    	assertEquals(JSONSerializer.toJSON(firstBBean, jsonConfig).toString(), 
-//    			Op.on(firstBBean).exec(ToJsonString.forObject(jsonConfig)).get());   
-//    	
-//	}
-//
-//	@Test
-//	public final void test5() {
-//		
-//		JsonConfig jsonConfig = new JsonConfig();
-//	
-//		ArrayList<Long> firstLongsList = new ArrayList<Long>();
-//    	firstLongsList.add(Long.valueOf(4));
-//    	firstLongsList.add(Long.valueOf(3));
-//    	firstLongsList.add(Long.valueOf(2));
-//    	firstLongsList.add(Long.valueOf(1));		
-//    	
-//    	ABean firstABean = new ABean("The string property value of ABean - firstBean", 
-//    			13, 
-//    			Integer.valueOf(22), 
-//    			firstLongsList, 
-//				new ArrayList<Object>(), 
-//				Calendar.getInstance());   
-//    	
-//    	ArrayList<Object> firstObjectsList = new ArrayList<Object>();
-//    	firstObjectsList.add(Long.valueOf(3));
-//    	firstObjectsList.add(firstABean);   
-//    	
-//    	
-//    	ArrayList<Long> secondLongsList = new ArrayList<Long>();
-//    	secondLongsList.add(Long.valueOf(34));
-//    	
-//    	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
-//    	secondObjectsList.add("one");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	secondObjectsList.add("A string property in the secondObjectsList");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(new ArrayList<Long>());
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	
-//    	BBean firstBBean = new BBean("bStringProperty", 
-//    			34, 
-//    			Integer.valueOf(4), 
-//    			secondLongsList, 
-//    			secondObjectsList, 
-//    			Calendar.getInstance(), 
-//    			"A transient string property", 
-//    			34, 
-//    			secondLongsList, 
-//    			Calendar.getInstance(), 
-//    			Calendar.getInstance());
-//    	
-//    	
-//    			
-//    	jsonConfig = new JsonConfig();
-//    	jsonConfig.setIgnoreTransientFields(true);
-//    	jsonConfig.setIgnoreJPATransient(true);
-//    	assertEquals(JSONSerializer.toJSON(firstBBean, jsonConfig).toString(), 
-//    			Op.on(firstBBean).exec(ToJsonString.forObject(jsonConfig)).get());   
-//    	    	 	
-//    	
-//	}
-//
-//	@Test
-//	public final void test6() {
-//		
-//		JsonConfig jsonConfig = new JsonConfig();
-//	
-//		ArrayList<Long> firstLongsList = new ArrayList<Long>();
-//    	firstLongsList.add(Long.valueOf(4));
-//    	firstLongsList.add(Long.valueOf(3));
-//    	firstLongsList.add(Long.valueOf(2));
-//    	firstLongsList.add(Long.valueOf(1));		
-//    	
-//    	ABean firstABean = new ABean("The string property value of ABean - firstBean", 
-//    			13, 
-//    			Integer.valueOf(22), 
-//    			firstLongsList, 
-//				new ArrayList<Object>(), 
-//				Calendar.getInstance());   
-//    	
-//    	ArrayList<Object> firstObjectsList = new ArrayList<Object>();
-//    	firstObjectsList.add(Long.valueOf(3));
-//    	firstObjectsList.add(firstABean);   
-//    	
-//    	ABean secondABean = new ABean("Test json - secondABean string property value", 
-//    			10, 
-//    			Integer.valueOf(5), 
-//    			firstLongsList,
-//				firstObjectsList, 
-//				Calendar.getInstance());
-//    	
-//    	ArrayList<Long> secondLongsList = new ArrayList<Long>();
-//    	secondLongsList.add(Long.valueOf(34));
-//    	
-//    	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
-//    	secondObjectsList.add("one");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	secondObjectsList.add("A string property in the secondObjectsList");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(new ArrayList<Long>());
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	
-//    	
-//    	
-//    	
-//    	jsonConfig = new JsonConfig();
-//    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
-//    	assertEquals(JSONObject.fromObject(secondABean, jsonConfig).toString(),
-//    			Op.on(secondABean).exec(ToJsonString.forObject(jsonConfig)).get());
-//    	
-//    	
-//    	
-//	}
-//	
-//	@Test
-//	public final void test7() {
-//		
-//		JsonConfig jsonConfig = new JsonConfig();
-//	
-//		ArrayList<Long> firstLongsList = new ArrayList<Long>();
-//    	firstLongsList.add(Long.valueOf(4));
-//    	firstLongsList.add(Long.valueOf(3));
-//    	firstLongsList.add(Long.valueOf(2));
-//    	firstLongsList.add(Long.valueOf(1));		
-//    	
-//    	ABean firstABean = new ABean("The string property value of ABean - firstBean", 
-//    			13, 
-//    			Integer.valueOf(22), 
-//    			firstLongsList, 
-//				new ArrayList<Object>(), 
-//				Calendar.getInstance());   
-//    	
-//    	ArrayList<Object> firstObjectsList = new ArrayList<Object>();
-//    	firstObjectsList.add(Long.valueOf(3));
-//    	firstObjectsList.add(firstABean);   
-//    	
-//    	ABean secondABean = new ABean("Test json - secondABean string property value", 
-//    			10, 
-//    			Integer.valueOf(5), 
-//    			firstLongsList,
-//				firstObjectsList, 
-//				Calendar.getInstance());
-//    	
-//    	ArrayList<Long> secondLongsList = new ArrayList<Long>();
-//    	secondLongsList.add(Long.valueOf(34));
-//    	
-//    	ArrayList<Object> secondObjectsList = new ArrayList<Object>();
-//    	secondObjectsList.add("one");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	secondObjectsList.add("A string property in the secondObjectsList");
-//    	secondObjectsList.add(Long.valueOf(45));
-//    	secondObjectsList.add(new ArrayList<Long>());
-//    	secondObjectsList.add(Calendar.getInstance());
-//    	
-//    	
-//    	
-//    	
-//    	jsonConfig = new JsonConfig();
-//    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
-//    	
-//    	assertEquals(JSONArray.fromObject(new ABean[] {secondABean, secondABean}, jsonConfig).toString(),
-//    			Op.on(Arrays.asList(new ABean[] {secondABean, secondABean})).exec(ToJsonString.forObject(jsonConfig)).get());
-//    	
-//    	
-//	}
+	@Test
+	public final void test4() {
+    			    	    	   	
+    	JsonConfig jsonConfig = new JsonConfig();
+    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
+    	jsonConfig.setIgnoreTransientFields(false);
+    	jsonConfig.setIgnoreJPATransient(false);
+    	assertEquals(JSONSerializer.toJSON(this.firstBBean, jsonConfig).toString(), 
+    			Op.on(this.firstBBean).exec(ToJsonString.fromObject(jsonConfig)).get());      	
+	}
+
+	@Test
+	public final void test5() {
+				
+    	JsonConfig jsonConfig = new JsonConfig();
+    	jsonConfig.setIgnoreTransientFields(true);
+    	jsonConfig.setIgnoreJPATransient(true);
+    	assertEquals(JSONSerializer.toJSON(this.firstBBean, jsonConfig).toString(), 
+    			Op.on(this.firstBBean).exec(ToJsonString.fromObject(jsonConfig)).get());      	
+	}
+
+	@Test
+	public final void test6() {
+		
+		JsonConfig jsonConfig = new JsonConfig();
+    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
+    	assertEquals(JSONObject.fromObject(this.secondABean, jsonConfig).toString(),
+    			Op.on(this.secondABean).exec(ToJsonString.fromObject(jsonConfig)).get());
+	}
+	
+	@Test
+	public final void test7() {
+		
+		JsonConfig jsonConfig = new JsonConfig();
+    	jsonConfig.registerJsonValueProcessor(Calendar.class, new CalendarJsonValueProcessor());
+    	
+    	assertEquals(JSONArray.fromObject(new ABean[] {this.secondABean, this.secondABean}, jsonConfig).toString(),
+    			Op.on(Arrays.asList(new ABean[] {this.secondABean, this.secondABean}))
+    			.exec(ToJsonString.fromObject(jsonConfig)).get());   	
+	}
 }
